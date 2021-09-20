c975886e6742f97490ff66edde754f36836c21c2
Morten Johansson <morten.johansson@outlook.com>
2021-09-20 17:17:05 +0200

    added google api script in index. Added a temporary footer with basic styles. Started working on the maps section functionality.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+// Google maps
+function initMap() {
+  let location = {
+    lat: -50.606704,
+    lng: 165.972467,
+  };
+
+  let options = {
+    center: location,
+    zoom: 15,
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (loc) => {
+        location.lat = loc.coords.latitude;
+        location.lng = loc.coords.longitude;
+
+        map = new google.maps.Map(document.getElementById("map"), options);
+      },
+      (err) => {
+        map = new google.maps.Map(document.getElementById("map"), options);
+      }
+    );
+  } else {
+    map = new google.maps.Map(document.getElementById("map"), options);
+  }
+}
